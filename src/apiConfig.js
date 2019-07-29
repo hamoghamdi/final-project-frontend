c11845b772f3a6d6a7be03cfa95215dd110f8d42
hamoghamdi <haneen.aljabrah@gmail.com>
2019-07-29 15:53:23 +0300

    backend deployment link - development

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,9 +1,9 @@
 let apiUrl
 const expressPort = 3000
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: "https://pure-refuge-47274.herokuapp.com/",
   development: `http://localhost:${expressPort}`
-}
+};
 
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
